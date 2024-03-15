8d0fa304dd16b0a4b45128bfbc77ef94a5d1cd97
amine001 <aminefathi@tutanota.com>
2024-03-15 15:40:38 +0000

    Refactor authentication middleware and add API key validation

--- server/src/helpers/validator.ts
+++ server/src/helpers/validator.ts
@@ -1,4 +1,38 @@
-import joi from 'joi';
+import Joi from 'joi';
 import { Request, Response, NextFunction } from 'express';
 import { BadRequestError } from '@/core/ApiError';
-import { Types } from 'mongoose';
+
+export enum ValidationSource {
+  BODY = 'body',
+  HEADER = 'headers',
+  QUERY = 'query',
+  PARAM = 'params',
+}
+
+export const JoiAuthBearer = () =>
+  Joi.string().custom((value: string, helpers) => {
+    if (!value.startsWith('Bearer ')) return helpers.error('any.invalid');
+    if (!value.split(' ')[1]) return helpers.error('any.invalid');
+    return value;
+  }, 'Authorization Header Validation');
+
+export default (
+    schema: Joi.AnySchema,
+    source: ValidationSource = ValidationSource.BODY,
+  ) =>
+  (req: Request, res: Response, next: NextFunction) => {
+    try {
+      const { error } = schema.validate(req[source]);
+
+      if (!error) return next();
+
+      const { details } = error;
+      const message = details
+        .map((i) => i.message.replace(/['"]+/g, ''))
+        .join(',');
+
+      next(new BadRequestError(message));
+    } catch (error) {
+      next(error);
+    }
+  };
--- server/src/routes/index.ts
+++ server/src/routes/index.ts
@@ -1,7 +1,17 @@
 // index of all api/vi Routes gor bettre app structure
 
 import express from 'express';
+import apikey from '../auth/apikey';
+import permission from '@/helpers/permission';
+import { Permission } from '@/database/model/Apikey';
 
 const router = express.Router();
+
+/*---------------------------------------------------------*/
+router.use(apikey);
+/*---------------------------------------------------------*/
+/*---------------------------------------------------------*/
+router.use(permission(Permission.GENERAL));
+/*---------------------------------------------------------*/
 
 export default router;
